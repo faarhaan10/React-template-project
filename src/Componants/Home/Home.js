a5768049a883b3877752d0e9b0afb813ce3cf720
Farhan <farhan@Farhans-MacBook-Air.local>
2023-02-01 21:43:24 +0600

    client app

---
+++ src/Componants/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>hello</h1>
+            <h1>welcome to home</h1>
+        </div>
+    );
+};
+
+export default Home;
